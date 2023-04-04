8cfaef34c79e265f910e6c68119adcabf6fec3e8
Sam Moore <sammooresemail23@gmail.com>
2023-04-04 11:25:20 -0500

    samsbranch test

---
+++ README.md
@@ -0,0 +1,20 @@
+# ReelsReelsReels
+
+User Story for ReelsReelsReels 
+
+* As a user I want to search for movies and get details about the movie with access to a trailer.
+* This application uses youtube API and OMDB API
+* 
+ 
+
+##  Links
+
+[Git Page](https://daniel-covington.github.io/ReelsReelsReels/)
+
+[Git Hub](https://github.com/Daniel-Covington/ReelsReelsReels)
+
+## Preview
+
+The following image demonstrates the web application's appearance and functionality:
+
+![Preview of Website(Desktop)](./assets/images/Preview.png)
---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+async function searchMovie() {
+  const searchInput = document.getElementById('search-input');
+  const movieTitle = searchInput.value;
+  const omdbUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=a23c4e83`;
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&q=${encodeURIComponent(movieTitle)}%20trailer&key=AIzaSyB5rvZMTGTsNN0LtOiGn_vGJMd1_n7VLV0`;
+  try {
+    const omdbResponse = await fetch(omdbUrl);
+    const omdbData = await omdbResponse.json();
+
+    if (omdbData.Response === 'True') {
+      displayMovieDetails(omdbData);
+      
+      const youtubeResponse = await fetch(youtubeUrl);
+      const youtubeData = await youtubeResponse.json();
+      const videoId = youtubeData.items[0].id.videoId;
+      displayTrailer(videoId);
+    } else {
+      alert('Movie not found.');
+    }
+    const youtubeResponse = await fetch(youtubeUrl);
+    const youtubeData = await youtubeResponse.json();
+    displaySearchResults(youtubeData.items);
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+function displaySearchResults(searchResults) {
+  const searchResultsContainer = document.getElementById('search-results');
+  let searchResultThumbnails = '';
+
+  searchResults.forEach(result => {
+    searchResultThumbnails += `
+      <div style="display: inline-block; margin: 5px;">
+        <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
+          <img src="${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}">
+          
+        </a>
+      </div>
+    `;
+  });
+
+  searchResultsContainer.innerHTML = searchResultThumbnails;
+}
+
+
+function displayMovieDetails(movieData) {
+  const movieDetails = document.getElementById('movie-details');
+  movieDetails.innerHTML = `
+    <h2>${movieData.Title} (${movieData.Year})</h2>
+    <p>Director: ${movieData.Director}</p>
+    <p>Actors: ${movieData.Actors}</p>
+    <p>Plot: ${movieData.Plot}</p>
+  `;
+}
+
+
+function displayTrailer(videoId) {
+  const movieTrailer = document.getElementById('movie-trailer');
+  movieTrailer.innerHTML = `
+    <h2>Trailer</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  `;
+}
+
+// Load in top current trailers at page load
+async function fetchPopularTrailers() {
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&q=official%20trailer&order=viewCount&videoDefinition=high&publishedAfter=2022-01-01T00:00:00Z&key=AIzaSyB5rvZMTGTsNN0LtOiGn_vGJMd1_n7VLV0`;
+
+  try {
+    const youtubeResponse = await fetch(youtubeUrl);
+    const youtubeData = await youtubeResponse.json();
+    displayPopularTrailers(youtubeData.items);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+function displayPopularTrailers(trailers) {
+  const trailersContainer = document.getElementById('trailers-container');
+  let trailerThumbnails = '';
+
+  trailers.forEach(trailer => {
+    trailerThumbnails += `
+      <div style="display: inline-block; margin: 5px;">
+        <a href="https://www.youtube.com/watch?v=${trailer.id}" target="_blank">
+          <img src="${trailer.snippet.thumbnails.medium.url}" alt="${trailer.snippet.title}">
+        </a>
+      </div>
+    `;
+  });
+
+  trailersContainer.innerHTML = trailerThumbnails;
+}
