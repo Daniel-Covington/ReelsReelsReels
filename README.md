79992e848f1cea2890412db03e447eac1b525025
Daniel Covington <DanielmCovington@gmail.com>
2023-04-09 16:33:23 -0500

    Updated readme with features, getting started, tech used, and future dev. Created a site preview image. Updated formatting on index. Fixed a bug in JS that when user clicked on a thumbnail, when it went to youtube it said video not available. Created a variable for youtube API key for rapid changing if needed, and updated youtube API keys to be varible. Updated JS formatting. Removed blank spaces in JS and css.

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # ReelsReelsReels
 
-User Story for ReelsReelsReels 
+ReelsReelsReels is a web application that allows users to search for movie trailers and related videos.
+With an easy-to-use interface, it provides movie details, popular movie trailers, and related search 
+results from YouTube, all in one place.
 
-* As a user I want to search for movies and get details about the movie with access to a trailer.
-* This application uses youtube API and OMDB API
-* 
- 
+## Features
+
+* Search for movie trailers by entering a movie title.
+* View movie details such as director, actors, and plot description.
+* Watch movie trailers in an embedded video player.
+* Browse related videos based on the searched movie title.
+* Explore the most popular movie trailers at the time of page load.
+* Save search history locally for easy access to previous searches.
+
+## Getting Started
+To set up ReelsReelsReels on your local machine, follow these instructions:
+
+* Clone the repository.
+* Install the required dependencies.
+* Obtain API keys from Obtain [OMDb API](https://www.omdbapi.com/) and [Google APIs](https://developers.google.com/youtube/v3).
+* Replace the placeholders in the code with your API keys.
+* Open the index.html file in your favorite browser.
+
+
+## Technologies Used 
+* HTML
+* CSS
+* JavaScript
+* OMDb API
+* YouTube Data API v3
+
+## Future Development
+* For the related and popular trailers thumbnails, we would like for it to pass the trailer name from the youtube API to the search function with a click listen event, so it would be processed as if it was a searched trailer that hits omdb api and displays the trailer as the embedded main video.
 
 ##  Links
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,40 @@
+const YouTubeapiKey = 'AIzaSyA_uglFhReUs61gcPEwXhfwwFsb31sfVhM';
+
 // Elements for the Modal Y'all //
 var modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+
   }
-}
+};
 
 async function searchMovie() {
-  const searchInput = document.getElementById('search-input');
+  const searchInput = document.getElementById("search-input");
   const movieTitle = searchInput.value;
-  const omdbUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=a23c4e83`;
-  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=03&type=video&q=${encodeURIComponent(movieTitle)}%20trailer&key=AIzaSyA_uglFhReUs61gcPEwXhfwwFsb31sfVhM`;
-    // Save the search history and update the datalist
+  const omdbUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(
+    movieTitle
+  )}&apikey=a23c4e83`;
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=03&type=video&q=${encodeURIComponent(
+    movieTitle
+  )}%20trailer&key=${YouTubeapiKey}`;
+
   try {
     const omdbResponse = await fetch(omdbUrl);
     const omdbData = await omdbResponse.json();
 
-    if (omdbData.Response === 'True') {
+    if (omdbData.Response === "True") {
       displayMovieDetails(omdbData);
-      
+
       const youtubeResponse = await fetch(youtubeUrl);
       const youtubeData = await youtubeResponse.json();
       const videoId = youtubeData.items[0].id.videoId;
@@ -38,27 +45,28 @@ async function searchMovie() {
     const youtubeResponse = await fetch(youtubeUrl);
     const youtubeData = await youtubeResponse.json();
     displaySearchResults(youtubeData.items);
-
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
   saveSearchHistory(movieTitle);
   updateSearchHistoryDatalist();
 }
+
+//Save search history function
 function saveSearchHistory(searchTerm) {
-  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
   if (!searchHistory.includes(searchTerm)) {
     searchHistory.push(searchTerm);
-    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
   }
 }
 
 function updateSearchHistoryDatalist() {
-  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-  const searchHistoryDatalist = document.getElementById('search-history');
-  let searchHistoryOptions = '';
+  const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  const searchHistoryDatalist = document.getElementById("search-history");
+  let searchHistoryOptions = "";
 
-  searchHistory.forEach(term => {
+  searchHistory.forEach((term) => {
     searchHistoryOptions += `<option value="${term}">`;
   });
 
@@ -66,7 +74,9 @@ function updateSearchHistoryDatalist() {
 }
 
 function updateSearchHistory() {
-  const searchHistory = JSON.parse(localStorage.getItem("searchHistory") || "[]");
+  const searchHistory = JSON.parse(
+    localStorage.getItem("searchHistory") || "[]"
+  );
   const searchHistoryDatalist = document.getElementById("search-history");
   searchHistoryDatalist.innerHTML = "";
 
@@ -77,16 +87,17 @@ function updateSearchHistory() {
   });
 }
 
-// Call updateSearchHistory() when the page loads
+// Call updateSearchHistory function when the page loads
 document.addEventListener("DOMContentLoaded", () => {
   updateSearchHistory();
 });
 
+//display related search results to user sumbitted movie title function
 function displaySearchResults(searchResults) {
-  const searchResultsContainer = document.getElementById('search-results');
-  let searchResultThumbnails = '';
+  const searchResultsContainer = document.getElementById("search-results");
+  let searchResultThumbnails = "";
 
-  searchResults.forEach(result => {
+  searchResults.forEach((result) => {
     searchResultThumbnails += `
       <div style="display: inline-block; margin: 20px;">
         <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
@@ -99,8 +110,9 @@ function displaySearchResults(searchResults) {
   searchResultsContainer.innerHTML = searchResultThumbnails;
 }
 
+//display details from omdb API function
 function displayMovieDetails(movieData) {
-  const movieDetails = document.getElementById('movie-details');
+  const movieDetails = document.getElementById("movie-details");
   movieDetails.innerHTML = `
     <h2 class= "titles-0" >${movieData.Title} (${movieData.Year})</h2>
     <p class= "titles-1">
@@ -114,37 +126,38 @@ function displayMovieDetails(movieData) {
     </p>
   `;
 }
-
+//display searched trailer with embedded video function
 function displayTrailer(videoId) {
-  const movieTrailer = document.getElementById('movie-trailer');
+  const movieTrailer = document.getElementById("movie-trailer");
   movieTrailer.innerHTML = `
     <h2 class= "texttrailer">Trailer</h2>
     <iframe class="responsive-iframe" width="1200" height="620" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <h2 class= "texttrailer">Related Videos</h2>
+    <h2 class= "texttrailer">Related Trailers</h2>
   `;
 }
 
 // Load in top current trailers at page load
 async function fetchPopularTrailers() {
-  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=06&type=video&q=official%20trailer&order=viewCount&videoDefinition=high&publishedAfter=2022-01-01T00:00:00Z&key=AIzaSyA_uglFhReUs61gcPEwXhfwwFsb31sfVhM`;
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=06&type=video&q=official%20trailer&order=viewCount&videoDefinition=high&publishedAfter=2022-01-01T00:00:00Z&key=${YouTubeapiKey}`;
 
   try {
     const youtubeResponse = await fetch(youtubeUrl);
     const youtubeData = await youtubeResponse.json();
     displayPopularTrailers(youtubeData.items);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 }
 
+// Display trailers as thumbnails function
 function displayPopularTrailers(trailers) {
-  const trailersContainer = document.getElementById('trailers-container');
-  let trailerThumbnails = '';
+  const trailersContainer = document.getElementById("trailers-container");
+  let trailerThumbnails = "";
 
-  trailers.forEach(trailer => {
+  trailers.forEach((trailer) => {
     trailerThumbnails += `
       <div style="display: inline-block; margin: 20px;">
-        <a href="https://www.youtube.com/watch?v=${trailer.id}" target="_blank">
+        <a href="https://www.youtube.com/watch?v=${trailer.id.videoId}" target="_blank">
           <img class="trailer-thumbnail" src="${trailer.snippet.thumbnails.medium.url}" alt="${trailer.snippet.title}">
         </a>
       </div>
@@ -153,7 +166,3 @@ function displayPopularTrailers(trailers) {
 
   trailersContainer.innerHTML = trailerThumbnails;
 }
-
-
-
-
